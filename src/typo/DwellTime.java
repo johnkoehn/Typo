742e545f8f2a5e92866bffdbd5ec50c5b6d385fd
John Koehn <kingkoehn@gmail.com>
2016-02-20 11:47:35 -0600

    began implementation of dwelltime

--- src/typo/DwellTime.java
+++ src/typo/DwellTime.java
@@ -2,7 +2,69 @@ package typo;
 
 public class DwellTime
 {
-	public DwellTime(int id)
+	private int id;
+	private int masterId;
+	int times[] = new int[5];
+	int avg_time = 0;
+	boolean first = true;
+	int index;
+	
+	public DwellTime(int id, int masterId)
+	{
+		this.id = id;
+		this.masterId = masterId;
+		index = times.length;
+	}
+	
+	public void receive(int time)
+	{
+		//check if this is the first time received
+		if(!first)
+		{
+			for(int i = 0; i < times.length - 1; i++)
+			{
+				times[i] = times[i+1];
+			}
+			if(index > 0)
+			{
+				index -= 1;
+			}
+			avg();
+
+		}
+		else
+		{
+			times[4] = time;
+			avg_time = time;
+			first = false;
+			index += 1;
+		}
+	}
+	
+	private void avg()
+	{
+		int sum = 0;
+		int count = 0;
+		for(int i = times.length; i >= index; i--)
+		{
+			sum += times[i];
+			count += 1;
+		}
+		
+		avg_time = sum/count;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public int getTime()
+	{
+		return avg_time;
+	}
+	
+	public void write()
 	{
 		
 	}

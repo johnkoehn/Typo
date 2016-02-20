ffc193da58fa6664768bf91c930fef6f87f12524
Jacob Feldman <feldman@iastate.edu>
2016-02-20 12:26:36 -0600

    dwelltimemanager

--- src/typo/DwellTimeManager.java
+++ src/typo/DwellTimeManager.java
@@ -1,5 +1,8 @@
 package typo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import org.jnativehook.keyboard.NativeKeyEvent;
@@ -7,6 +10,8 @@ import org.jnativehook.keyboard.NativeKeyEvent;
 public class DwellTimeManager
 {
 	private ArrayList<DwellTime> dTimes;
+	
+	private int masterId;
 	
 	public DwellTimeManager(int masterId){
 		dTimes.add(new DwellTime(NativeKeyEvent.VC_A, masterId));
@@ -36,6 +41,8 @@ public class DwellTimeManager
 		dTimes.add(new DwellTime(NativeKeyEvent.VC_Y, masterId));
 		dTimes.add(new DwellTime(NativeKeyEvent.VC_Z, masterId));
 		dTimes.add(new DwellTime(NativeKeyEvent.VC_SPACE, masterId));
+		
+		this.masterId = masterId;
 	}
 	
 	public void pressed(int id, int time){
@@ -48,13 +55,116 @@ public class DwellTimeManager
 		}
 	}
 	
-	public void write(){
+	public boolean write(String user){
 		//loop through the array, everything is going to call .write()
-		for(int i = 0; i < dTimes.size(); i += 1){
-			dTimes.get(i).write();
+		PrintWriter write;
+		File file;
+		switch(masterId){
+			case(NativeKeyEvent.VC_A):
+				file = new File(user + "_A");
+				break;
+			case(NativeKeyEvent.VC_B):
+				file = new File(user + "_B");
+				break;
+			case(NativeKeyEvent.VC_C):
+				file = new File(user + "_C");
+				break;
+			case(NativeKeyEvent.VC_D):
+				file = new File(user + "_D");
+				break;
+			case(NativeKeyEvent.VC_E):
+				file = new File(user + "_E");
+				break;
+			case(NativeKeyEvent.VC_F):
+				file = new File(user + "_F");
+				break;
+			case(NativeKeyEvent.VC_G):
+				file = new File(user + "_G");
+				break;
+			case(NativeKeyEvent.VC_H):
+				file = new File(user + "_H");
+				break;
+			case(NativeKeyEvent.VC_I):
+				file = new File(user + "_I");
+				break;
+			case(NativeKeyEvent.VC_J):
+				file = new File(user + "_J");
+				break;
+			case(NativeKeyEvent.VC_K):
+				file = new File(user + "_K");
+				break;
+			case(NativeKeyEvent.VC_L):
+				file = new File(user + "_L");
+				break;
+			case(NativeKeyEvent.VC_M):
+				file = new File(user + "_M");
+				break;
+			case(NativeKeyEvent.VC_N):
+				file = new File(user + "_N");
+				break;
+			case(NativeKeyEvent.VC_O):
+				file = new File(user + "_O");
+				break;
+			case(NativeKeyEvent.VC_P):
+				file = new File(user + "_P");
+				break;
+			case(NativeKeyEvent.VC_Q):
+				file = new File(user + "_Q");
+				break;
+			case(NativeKeyEvent.VC_R):
+				file = new File(user + "_R");
+				break;
+			case(NativeKeyEvent.VC_S):
+				file = new File(user + "_S");
+				break;
+			case(NativeKeyEvent.VC_T):
+				file = new File(user + "_T");
+				break;
+			case(NativeKeyEvent.VC_U):
+				file = new File(user + "_U");
+				break;
+			case(NativeKeyEvent.VC_V):
+				file = new File(user + "_V");
+				break;
+			case(NativeKeyEvent.VC_W):
+				file = new File(user + "_W");
+				break;
+			case(NativeKeyEvent.VC_X):
+				file = new File(user + "_X");
+				break;
+			case(NativeKeyEvent.VC_Y):
+				file = new File(user + "_Y");
+				break;
+			case(NativeKeyEvent.VC_Z):
+				file = new File(user + "_Z");
+				break;
+			case(NativeKeyEvent.VC_SPACE):
+				file = new File(user + "_SP");
+				break;
+			default:
+				return false;
 		}
+		try
+		{
+			write = new PrintWriter(file);
+			for(int i = 0; i < dTimes.size(); i += 1){
+				write.println(dTimes.get(i).write());
+		
+			}
+			write.close();
+		} catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+		
+		return true;
 	}
 	
+	
+	//-1 = not recording key
 	public int getTime(int id){
 		for(int i = 0; i < dTimes.size(); i += 1){
 			if(dTimes.get(i).getId() == id){
